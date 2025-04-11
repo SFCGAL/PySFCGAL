c6354f10ed5079c47cac25a1b7826e5df0a39813
Jean Felder <jean.felder@oslandia.com>
2025-04-11 11:02:40 +0200

    chore(ci): Check the generated wheels with twine

--- ci/build.ps1
+++ ci/build.ps1
@@ -2,7 +2,7 @@
 Invoke-WebRequest -Uri "https://download.osgeo.org/osgeo4w/osgeo4w-setup-v2.exe" -OutFile "osgeo4w-setup-v2.exe"
 Start-Process -FilePath "osgeo4w-setup-v2.exe" -ArgumentList "--advanced --autoaccept --delete-orphans --no-desktop --quiet-mode --upgrade-also --site https://download.osgeo.org/osgeo4w/v2 --packages python3-core,python3-pip,python3-devel" -Wait
 Remove-Item -Path .\osgeo4w-setup-v2.exe -Force
-C:\OSGeo4W\OSGeo4W.bat python3 -m pip install -U pip build delvewheel
+C:\OSGeo4W\OSGeo4W.bat python3 -m pip install -U pip build delvewheel twine
 
 # Generate the wheel
 $env:INCLUDE='c:\SFCGAL\build\include\'
@@ -15,3 +15,6 @@ C:\OSGeo4W\OSGeo4W.bat delvewheel repair --add-path $env:LIB -w dist $env:WHEEL_
 
 # Rename dist directory to prevent issues with build from other os
 Rename-Item -Path "dist" -NewName "dist_windows"
+
+# check the generated wheel with twine
+C:\OSGeo4W\OSGeo4W.bat twine check --strict dist_windows/*
