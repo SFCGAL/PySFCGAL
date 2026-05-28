75ccc0838aa4d00afefc09fba165a6a0faa1a98d
Jean Felder <jean.felder@oslandia.com>
2026-05-28 16:29:36 +0200

    refactor(buffer): Directly use c lib in BufferType

--- pysfcgal/geometry/buffer.py
+++ pysfcgal/geometry/buffer.py
@@ -8,13 +8,15 @@ required in the Point case).
 from enum import IntEnum
 from typing import Type
 
+from .._sfcgal import lib
+
 
 class BufferType(IntEnum):
     label: str
 
-    SFCGAL_BUFFER3D_ROUND = 0, "Round"
-    SFCGAL_BUFFER3D_CYLSPHERE = 1, "CylSphere"
-    SFCGAL_BUFFER3D_FLAT = 2, "Flat"
+    SFCGAL_BUFFER3D_ROUND = lib.SFCGAL_BUFFER3D_ROUND, "Round"
+    SFCGAL_BUFFER3D_CYLSPHERE = lib.SFCGAL_BUFFER3D_CYLSPHERE, "CylSphere"
+    SFCGAL_BUFFER3D_FLAT = lib.SFCGAL_BUFFER3D_FLAT, "Flat"
 
     def __new__(cls: Type["BufferType"], value: int, label: str) -> "BufferType":
         obj = int.__new__(cls, value)
