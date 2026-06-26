1721e3c11f4b53477959c93d7cd8accfbaf58280
Jean Felder <jean.felder@oslandia.com>
2026-06-26 18:09:58 +0200

    build: Use pkg-config

--- pysfcgal/sfcgal_build.py
+++ pysfcgal/sfcgal_build.py
@@ -1,6 +1,59 @@
 import os
 import platform
+import subprocess
+
 from cffi import FFI
+
+
+def _pkg_config_available():
+    """Check once whether pkg-config finds SFCGAL."""
+    try:
+        subprocess.run(
+            ["pkg-config", "--exists", "sfcgal"],
+            capture_output=True, check=True
+        )
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+def _pkg_config(option):
+    try:
+        result = subprocess.run(
+            ["pkg-config", option, "sfcgal"],
+            capture_output=True, text=True, check=True
+        )
+        return result.stdout.strip().split()
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return []
+
+
+_USE_PKG_CONFIG = _pkg_config_available()
+
+
+def _get_include_dirs():
+    if _USE_PKG_CONFIG:
+        return [flag[2:] for flag in _pkg_config("--cflags-only-I")]
+    elif "INCLUDE_PATH" in os.environ:
+        return os.environ["INCLUDE_PATH"].split(os.pathsep)
+    return []
+
+
+def _get_library_dirs():
+    if _USE_PKG_CONFIG:
+        return [f[2:] for f in _pkg_config("--libs-only-L")]
+    elif "LIBPATH" in os.environ:
+        return os.environ["LIBPATH"].split(os.pathsep)
+    return []
+
+
+def _get_libraries():
+    if _USE_PKG_CONFIG:
+        return [f[2:] for f in _pkg_config("--libs-only-l")]
+    elif "SFCGAL_LIBNAME" in os.environ:
+        return [os.environ["SFCGAL_LIBNAME"]]
+    return ["SFCGAL"]
+
 
 ffibuilder = FFI()
 
@@ -10,15 +63,9 @@ ffibuilder.set_source(
 #include <stdlib.h>
 #include <SFCGAL/capi/sfcgal_c.h>
 """,
-    libraries=[
-        "SFCGAL" if "SFCGAL_LIBNAME" not in os.environ else os.environ["SFCGAL_LIBNAME"]
-    ],
-    library_dirs=[]
-    if "LIBPATH" not in os.environ
-    else os.environ["LIBPATH"].split(os.pathsep),
-    include_dirs=[]
-    if "INCLUDE_PATH" not in os.environ
-    else os.environ["INCLUDE_PATH"].split(os.pathsep),
+    libraries=_get_libraries(),
+    library_dirs=_get_library_dirs(),
+    include_dirs=_get_include_dirs(),
 )
 
 # Required until Alpha Shapes bug is not fixed on MSVC
